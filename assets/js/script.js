e7317cce1ab4980b36abde108e1d635be80cfc25
Amrit Singh <direcode@gmail.com>
2015-06-23 23:28:02 -0500

    code for adding list items is now working perfectly, close buttons are added on each list item but need to code these to make them work.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,73 @@
 
+(function(){
 
-var clickGo = document.querySelector('.button-go');
-var searchInput = document.querySelector('.search-input');
-var list = document.querySelector('.list');
-var listArray = [];
-/*
-clickGo.onclick = function() {
-  var searchValue = searchInput.value;
-    if(searchValue === ""){
-      alert("please put a value in the input field");
+    var clickGo = document.querySelector('.button-go');
+    var searchInput = document.querySelector('.search-input');
+    var list = document.querySelector('.list');
+    var listArray = [];
 
-      }else{
-        //checking if array already contains added list item
-        //if it is not, push the item to the array
-            if(listArray.indexOf(searchValue) == -1){
-              listArray.push(searchValue);
+    var clickGoFunc = function() {
+            var searchValue = searchInput.value;
 
-            // Adding li tags to the array list items
-            var liTag = document.createElement('li');
-
-            for(var i= 0; i < listArray.length; i++ ){
-              containerContent.textContent = (listArray[i]);
-
-              containerContent.appendChild(liTag);
-            }
-            // if arry contains already contains list item then alert following message
+            // check if input field is empty
+            if(searchValue === ""){
+                
+                alert("please put a value in the input field");
+            
             }else{
-              alert("value already exisits");
+
+                // check if list item already exists in the array named listArray
+                if(listArray.indexOf(searchValue) == -1){
+                    listArray.push(searchValue);
+
+                        // Function for creating loop for added array items and enclosing each array item inside <li>
+                        //Also adding a close button to each array item by adding <a> Tag
+                        var runIt = function (){
+                                for(var i= 0; i < listArray.length; i++ ){
+                                    var liTag = document.createElement('li');
+                                    var removeItem = document.createElement('span');
+                                    var closeLink = document.createElement('a');
+                                    closeLink.className = "close-link";
+                                    closeLink.setAttribute('href', "#");
+                                    closeLink.textContent = 'X';
+
+                                    // adding <a> tag inside span
+                                    removeItem.appendChild(closeLink);
+
+                                    liTag.textContent = listArray[i].toUpperCase();
+                                    list.appendChild(liTag);
+                                    liTag.appendChild(removeItem);
+                                    list.appendChild(liTag)
+                                } 
+                                // end of for loop
+                        }; 
+                        // end of runit
+
+                        // clearing ul.list container before reloading array items.
+                        list.innerHTML = "";
+
+                        // calling runIt funtion which contains for loop  for all the array items
+                        list.appendChild(runIt());
+
+                }else{
+                    alert("value already exisits");
+                } 
+                // end of if and else statement which is checking if value is already there is the array
+                // pushing new values in the listArray array 
+                // wrapping new array items in li tags and adding close button on each array item
             }
-          }
-        };
+            // if statement checking if the input field is empty , if not it runs other function which checks following 
+                    // end of if and else statement which is checking if value is already there is the array
+                    // pushing new values in the listArray array 
+                    // wrapping new array items in li tags and adding close button on each array item
+            } 
+            // end of clickGoFunc
 
-    */
 
+    //calling function 
+    clickGo.addEventListener('click', function(){
+            clickGoFunc()
+    });
 
-    clickGo.onclick = function() {
-        var searchValue = searchInput.value;
-        if(searchValue === ""){
-            alert("please put a value in the input field");
-        }else{
+}());
 
-            if(listArray.indexOf(searchValue) == -1){
-                listArray.push(searchValue);
-
-                var ulTag = document.createElement('ul');
-                
-                
-                for(var i= 0; i < listArray.length; i++ ){
-                    var liTag = document.createElement('li');
-                    var removeItem = document.createElement('span');
-                var closeLink = document.createElement('a');
-                closeLink.setAttribute('href', "#");
-                closeLink.textContent = 'X';
-                
-                // adding a tag inside span
-                removeItem.appendChild(closeLink);
-                
-                    liTag.textContent = listArray[i];
-                    ulTag.appendChild(liTag);
-                    liTag.appendChild(removeItem);
-                }
-
-                list.innerHTML = "";
-                list.appendChild(ulTag)
-
-            }else{
-                alert("value already exisits");
-            }
-        }
-    };
