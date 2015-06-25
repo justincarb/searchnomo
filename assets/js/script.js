76e37f220626ef5b4f6c18a90c378350063628cc
Amrit Singh <direcode@gmail.com>
2015-06-25 12:31:52 -0500

    Cleaner code with comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,44 @@
     var searchInput = document.querySelector('.search-input');
     var list = document.querySelector('.list');
     var listArray = [];
+    var aClickBtn = aClickBtn;
+
+    
+// Loop function to show array items inside Li tag with remove button on the right side 
+    var dynElelmentFunc = function(){
+        for(var i= 0; i < listArray.length; i++ ){
+            var liTag = document.createElement('li');
+            var removeItem = document.createElement('span');
+            var closeLink = document.createElement('a');
+            closeLink.className = "close-link";
+            closeLink.setAttribute('href', "#");
+            closeLink.textContent = 'X';
+
+            // adding <a> tag inside span
+            removeItem.appendChild(closeLink);
+            
+            // adding each array Item  
+            liTag.textContent = listArray[i].toUpperCase();
+
+            // adding Li tags to each array item
+            list.appendChild(liTag);
+
+
+            // adding remove button 
+            liTag.appendChild(removeItem);
+
+
+        } 
+        // end of for loop
+
+
+         // var for a tag created dynamiclly to make it available outside scope
+            aClickBtn = document.getElementsByTagName('a');
+            return aClickBtn;
+
+
+    };
+    //end of dynElements 
 
     var clickGoFunc = function() {
             var searchValue = searchInput.value;
@@ -16,58 +54,42 @@
             
             }else{
 
-                // check if list item already exists in the array named listArray
-                if(listArray.indexOf(searchValue) == -1){
-                    listArray.push(searchValue);
+                        // check if list item already exists in the array named listArray
+                        if(listArray.indexOf(searchValue) == -1){
+                            listArray.push(searchValue);
 
-                        // Function for creating loop for added array items and enclosing each array item inside <li>
-                        //Also adding a close button to each array item by adding <a> Tag
-                        var runIt = function (){
-                                for(var i= 0; i < listArray.length; i++ ){
-                                    var liTag = document.createElement('li');
-                                    var removeItem = document.createElement('span');
-                                    var closeLink = document.createElement('a');
-                                    closeLink.className = "close-link";
-                                    closeLink.setAttribute('href', "#");
-                                    closeLink.textContent = 'X';
+                               //Calling dynElelmentFunc which contains loop to display array items
+                                var runIt = function (){
+                                    dynElelmentFunc();
+                                }; 
+                                // end of runit
 
-                                    // adding <a> tag inside span
-                                    removeItem.appendChild(closeLink);
+                                // clearing ul.list container before reloading array items.
+                                list.innerHTML = "";
 
-                                    liTag.textContent = listArray[i].toUpperCase();
-                                    list.appendChild(liTag);
-                                    liTag.appendChild(removeItem);
-                                    list.appendChild(liTag)
-                                } 
-                                // end of for loop
-                        }; 
-                        // end of runit
-
-                        // clearing ul.list container before reloading array items.
-                        list.innerHTML = "";
-
-                        // calling runIt funtion which contains for loop  for all the array items
-                        list.appendChild(runIt());
-
-                }else{
-                    alert("value already exisits");
-                } 
-                // end of if and else statement which is checking if value is already there is the array
-                // pushing new values in the listArray array 
-                // wrapping new array items in li tags and adding close button on each array item
+                                // calling runIt function
+                                list.appendChild(runIt());
+                        }else{
+                            alert("value already exisits");
+                        } 
+                        // end of if and else statement which is checking if value is already there is the array
+                        // pushing new values in the listArray array 
+                        // wrapping new array items in li tags and adding close button on each array item
             }
             // if statement checking if the input field is empty , if not it runs other function which checks following 
-                    // end of if and else statement which is checking if value is already there is the array
-                    // pushing new values in the listArray array 
-                    // wrapping new array items in li tags and adding close button on each array item
-            } 
+            // end of if and else statement which is checking if value is already there is the array
+            // pushing new values in the listArray array 
+            // wrapping new array items in li tags and adding close button on each array item
+    }; 
             // end of clickGoFunc
 
 
     //calling function 
     clickGo.addEventListener('click', function(){
-            clickGoFunc()
+            clickGoFunc();
     });
+
+
 
 }());
 
